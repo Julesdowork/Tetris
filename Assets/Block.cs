e1ba7a16f65d4f3171b9afe49e469bae8641eb9d
Julian McNeill <julesdowork@gmail.com>
2018-09-02 01:10:39 -0400

    Make simple block

---
+++ Assets/Block.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    const int width = 30;
+    const int height = 28;
+    RectTransform rectTransform;
+
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Debug.Log("Transform Position: " + rectTransform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void MoveDown()
+    {
+
+    }
+}
