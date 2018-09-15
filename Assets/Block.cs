e3a0dfea5880273aaeacab54ec31ad93e430bed6
Julian McNeill <julesdowork@gmail.com>
2018-09-15 18:57:19 -0400

    Create grid and blocks

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -7,6 +7,8 @@ public class Block : MonoBehaviour
     const float width = 30;
     const float height = 28;
 
+    public bool atRest = false;
+
     public float Width
     {
         get
@@ -26,6 +28,7 @@ public class Block : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
+        StartCoroutine(Fall());
 	}
 	
 	// Update is called once per frame
@@ -35,11 +38,40 @@ public class Block : MonoBehaviour
         {
             MoveDown();
         }
+
+        MoveHorizontal();
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided with " + collision.collider.name);
+        if (collision.collider.CompareTag("Boundary"))
+            atRest = true;
+    }
 
     void MoveDown()
     {
-        float newY = transform.position.y - 0.45f;
+        float newY = transform.position.y - 1f;
         transform.position = new Vector3(transform.position.x, newY, 0);
+    }
+
+    void MoveHorizontal()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            float direction = Input.GetAxisRaw("Horizontal");
+            float newX = transform.position.x + direction;
+            transform.position = new Vector3(newX, transform.position.y, 0);
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(1f);
+        while (!atRest)
+        {
+            MoveDown();
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
